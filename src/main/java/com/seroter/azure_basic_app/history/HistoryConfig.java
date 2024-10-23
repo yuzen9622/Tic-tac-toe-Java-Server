e8efebee2e313a0ab6ff6666abf42a0350615dd5
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-10-23 09:13:28 +0800

    update database

--- src/main/java/com/seroter/azure_basic_app/history/HistoryConfig.java
+++ src/main/java/com/seroter/azure_basic_app/history/HistoryConfig.java
@@ -1,7 +1,5 @@
 package com.seroter.azure_basic_app.history;
 
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +7,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HistoryConfig {
 
-	@Bean(name = "historyCommandLineRunner")
-	CommandLineRunner commandLineRunner(HistoryRepository repository) {
-		return args -> {
-			History oscar = new History(1L, 2L, new String[] {}, new String[] { "1", "O" }, new String[] {});
-			repository.saveAll(List.of(oscar));
+    @Bean(name = "historyCommandLineRunner")
+    CommandLineRunner commandLineRunner(HistoryRepository repository) {
+        return args -> {
+            // History oscar = new History(1L, 2L, new String[] {}, new String[] { "1", "O" }, new String[] {});
+            // repository.saveAll(List.of(oscar));
 
-		};
+        };
 
-	};
+    }
+;
 }
