ea17d6144b577b3baadd604cdb902a47a27d25df
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-11-24 22:29:29 +0800

    jsonIgnore更改成jsonProperty讀寫分離

--- src/main/java/com/seroter/azure_basic_app/user/User.java
+++ src/main/java/com/seroter/azure_basic_app/user/User.java
@@ -2,7 +2,7 @@ package com.seroter.azure_basic_app.user;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,96 +16,96 @@ import jakarta.persistence.Transient;
 @Table(name = "\"user\"")
 public class User {
 
-	@Id
-	@SequenceGenerator(name = "user_sequence", allocationSize = 1, sequenceName = "user_sequence")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
-	private Long id;
+    @Id
+    @SequenceGenerator(name = "user_sequence", allocationSize = 1, sequenceName = "user_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    private Long id;
 
-	private String name;
-	private String email;
-	@JsonIgnore
-	private String password;
-	@Transient
-	private LocalDate date;
+    private String name;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @Transient
+    private LocalDate date;
 
-	public User() {
-	}
+    public User() {
+    }
 
-	public User(Long id, String name, String email, String password) {
+    public User(Long id, String name, String email, String password) {
 
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
 
-	}
+    }
 
-	public User(Long id, String name, String email) {
+    public User(Long id, String name, String email) {
 
-		this.id = id;
-		this.name = name;
-		this.email = email;
+        this.id = id;
+        this.name = name;
+        this.email = email;
 
-	}
+    }
 
-	public User(String name, String email, String password) {
+    public User(String name, String email, String password) {
 
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-	public User(String name, String email) {
+    public User(String name, String email) {
 
-		this.name = name;
-		this.email = email;
+        this.name = name;
+        this.email = email;
 
-	}
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public LocalDate getDate() {
-		return LocalDate.now();
-	}
+    public LocalDate getDate() {
+        return LocalDate.now();
+    }
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", date=" + date
-				+ "]";
-	}
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", date=" + date
+                + "]";
+    }
 
 }
